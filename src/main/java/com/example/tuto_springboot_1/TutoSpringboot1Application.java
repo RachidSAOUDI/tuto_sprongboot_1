260983ea956a22428d0c63c92620a2a4ec8e705a
RachidSAOUDI <102986947+RachidSAOUDI@users.noreply.github.com>
2023-09-23 14:20:27 +0100

    Create TutoSpringboot1Application.java

---
+++ src/main/java/com/example/tuto_springboot_1/TutoSpringboot1Application.java
@@ -0,0 +1,13 @@
+package com.example.tuto_springboot_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutoSpringboot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutoSpringboot1Application.class, args);
+	}
+
+}
