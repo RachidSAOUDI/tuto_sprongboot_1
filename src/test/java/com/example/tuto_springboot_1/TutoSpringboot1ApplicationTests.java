8e4e410ee084d4a4359a574e586f37847bdedbde
RachidSAOUDI <102986947+RachidSAOUDI@users.noreply.github.com>
2023-09-23 14:24:25 +0100

    Create TutoSpringboot1ApplicationTests.java

---
+++ src/test/java/com/example/tuto_springboot_1/TutoSpringboot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tuto_springboot_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutoSpringboot1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
